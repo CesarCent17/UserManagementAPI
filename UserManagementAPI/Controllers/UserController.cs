752d0366f92106d0930b3fca75d1d05265380830
CesarCent <centurion_cesar1234@hotmail.com>
2023-03-07 15:17:03 -0500

    first commit

---
+++ UserManagementAPI/Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagementAPI.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
+
+namespace UserManagementAPI.Controllers
+{
+    [ApiController]
+    //[Route("[controller]")]
+    [Route("user")]
+    [EnableCors("AllowOrigin")]
+
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserController(IConfiguration configuration) 
+        {
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 27)));
+
+            _dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public IEnumerable<User> GetUsers()
+        {
+            return _dbContext.Users.ToList();
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            var user = _dbContext.Users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult CreateUser(User user)
+        {
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(user).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = _dbContext.Users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Users.Remove(user);
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ UserManagementAPI/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManagementAPI.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().ToTable("user");
+        }
+
+    }
+}
